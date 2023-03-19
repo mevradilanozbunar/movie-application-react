cd9e5fa3c4b5ad0f51e510f93a4325e229565a1c
Mevra Dilan Erten Özbunar <112767383+mevradilanozbunar@users.noreply.github.com>
2023-03-19 16:09:09 +0300

    commit

--- src/companents/SearchBar.js
+++ src/companents/SearchBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    }
+    
     render() {
         return (
 
-            <form>
+            <form onSubmit={this.handleFormSubmit}>
                 <div className="form-row mb-5">
                     <div className="col-12">
                         <input
-
-
+                            onChange={this.props.searchMovieProp} 
                             type="text" className="form-control"
                             placeholder="Seach a movie"
                         />
