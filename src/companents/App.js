c1f651b7ac2521d97e04ab08e9fabf62c4bb5f98
Mevra Dilan Erten Özbunar <112767383+mevradilanozbunar@users.noreply.github.com>
2023-03-19 19:18:37 +0300

    add axios

--- src/companents/App.js
+++ src/companents/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import axios from 'axios';
+
 
 class App extends React.Component {
 
@@ -9,13 +11,37 @@ class App extends React.Component {
         searchQuery: ""
     };
 
+    //get with fetch
+    // async componentDidMount() {
+    //     const response = await fetch("http://localhost:3002/movies");
+    //     const data = await response.json();
+    //     this.setState({ movies: data })
+    // }
 
-    deleteMovie = (movie) => {
+    //get with axios
+    async componentDidMount() {
+        const response=await axios.get("http://localhost:3002/movies");
+        this.setState({ movies: response.data })
+    }
 
-        const newMovieList=this.state.movies.filter(m => m.id !== movie.id)
-        this.setState(state => ({movies:newMovieList}))
+
+    //delete with fetch
+    // deleteMovie = async (movie) => {
+    //     await fetch(`http://localhost:3002/movies/${movie.id}`,{
+    //         method:"DELETE"
+    //     });
+    //     const newMovieList=this.state.movies.filter(m => m.id !== movie.id)
+    //     this.setState(state => ({movies:newMovieList}))
     
-    };
+    // };
+
+        //delete with axios
+        deleteMovie = async (movie) => {
+            await axios.delete(`http://localhost:3002/movies/${movie.id}`);           
+            const newMovieList=this.state.movies.filter(m => m.id !== movie.id)
+            this.setState(state => ({movies:newMovieList}))
+        
+        };
 
     SearchMovie = (event) => {
 
@@ -26,9 +52,7 @@ class App extends React.Component {
     render() {
 
       let filteredMovies=this.state.movies.filter((movie) => {return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !==-1
-      }).sort((a, b) => {
-        return a.id < b.id ? 1 : a.id > b.id ? -1 : 0;
-    });
+      }).sort((a, b) => { return a.id < b.id ? 1 : a.id > b.id ? -1 : 0;});
         return(
             <div>
                     <div className="container px-4">
